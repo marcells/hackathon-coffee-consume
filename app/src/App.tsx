dce46ba7358986bf3346b72ca2fcd6555a8c067a
Marcell Spies <m.spies@vescon.com>
2017-12-07 22:01:59 +0100

    Show users.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,46 @@ import './App.css';
 
 const logo = require('./logo.svg');
 
+class User {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+let users = [new User("David"), new User("Vladimir")]
+
+interface ICoffeeConsumeOverviewProps {
+  items: User[]
+}
+
+interface ICoffeeConsumeUserProps {
+  user: User
+}
+
+class CoffeeConsumeUser extends React.Component<ICoffeeConsumeUserProps> {
+  render() {
+    return (
+      <h1>{this.props.user.name}</h1>)
+  }
+}
+
+class CoffeeConsumeOverview extends React.Component<ICoffeeConsumeOverviewProps> {
+  render() {
+    return (
+      <table>
+        {this.props.items.map(user => (
+          <tr>
+            <td>
+              <CoffeeConsumeUser user={user} />
+            </td>
+          </tr>)
+        )}
+      </table>)
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
@@ -12,7 +52,7 @@ class App extends React.Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          <CoffeeConsumeOverview items={users} />
         </p>
       </div>
     );
