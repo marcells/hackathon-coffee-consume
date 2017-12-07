fcc21c1a704771aa85c1eefae38de3b329270f71
Marcell Spies <m.spies@vescon.com>
2017-12-07 22:42:26 +0100

    Added more stories.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,42 +4,74 @@ import './App.css';
 const logo = require('./logo.svg');
 
 class User {
-  name: string
+  readonly name: string;
+  numberOfDrunkCoffees: number = 0;
 
   constructor(name: string) {
-    this.name = name
+    this.name = name;
   }
 }
 
-let users = [new User("David"), new User("Vladimir")]
+let users = [new User('David'), new User('Vladimir')];
 
-interface ICoffeeConsumeOverviewProps {
-  items: User[]
+interface CoffeeConsumeOverviewProps {
+  items: User[];
 }
 
-interface ICoffeeConsumeUserProps {
-  user: User
+interface CoffeeConsumeUserProps {
+  user: User;
 }
 
-class CoffeeConsumeUser extends React.Component<ICoffeeConsumeUserProps> {
+class CoffeeConsumeUser extends React.Component<CoffeeConsumeUserProps> {
+  readonly incrementByOneCoffeeClick = () => {
+    this.props.user.numberOfDrunkCoffees++;
+
+    this.setState({});
+  }
+
+  readonly incrementByTwoCoffeesClick = () => {
+    this.props.user.numberOfDrunkCoffees++;
+    this.props.user.numberOfDrunkCoffees++;
+
+    this.setState({});
+  }
+
   render() {
     return (
-      <h1>{this.props.user.name}</h1>)
+      <div>
+        <span>{this.props.user.name}</span>
+        <span>{this.props.user.numberOfDrunkCoffees}</span>
+        <span><button onClick={this.incrementByOneCoffeeClick}>+</button></span>
+        <span><button onClick={this.incrementByTwoCoffeesClick}>++</button></span>
+      </div>);
   }
 }
 
-class CoffeeConsumeOverview extends React.Component<ICoffeeConsumeOverviewProps> {
+class CoffeeConsumeOverview extends React.Component<CoffeeConsumeOverviewProps> {
   render() {
     return (
-      <table>
-        {this.props.items.map(user => (
-          <tr>
-            <td>
-              <CoffeeConsumeUser user={user} />
-            </td>
-          </tr>)
-        )}
-      </table>)
+      <div>
+        {this.props.items.map(user => <CoffeeConsumeUser key={user.name} user={user} />)}
+      </div>);
+  }
+}
+
+class AddUser extends React.Component {
+  name: string;
+
+  readonly handleKeyPress = (e: any) => {
+    if (e.key === 'Enter') {
+      users.push(new User(this.name));
+
+      this.setState({});
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input placeholder="New user" value={this.name} onKeyPress={ this.handleKeyPress } />
+      </div>);
   }
 }
 
@@ -49,11 +81,12 @@ class App extends React.Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome, <br />dear Coffee-Consumer</h2>
         </div>
-        <p className="App-intro">
+        <div className="App-intro">
           <CoffeeConsumeOverview items={users} />
-        </p>
+        </div>
+        <AddUser />
       </div>
     );
   }
