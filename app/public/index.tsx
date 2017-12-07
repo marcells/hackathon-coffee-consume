2b5ed44265f19ae13954b40683b00fb271adf6c0
Marcell Spies <m.spies@vescon.com>
2017-12-07 21:32:14 +0100

    Added sample react/ts app.

--- app/public/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-
-class Game extends React.Component { }
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-)
